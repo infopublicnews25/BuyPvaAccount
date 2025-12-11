7c879c1564cdc3bb1640350eaa475a2e7c661b98
infopublicnews25 <infopublicnews25@gmail.com>
2025-12-11 07:42:09 -0800

    Use server.js instead of incomplete server-production.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: 'buypvaaccount',
-    script: 'backend/server-production.js',
+    script: 'backend/server.js',
     instances: 1,
     autorestart: true,
     watch: false,
