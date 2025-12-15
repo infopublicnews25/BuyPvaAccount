166bf72efd194fa4e3e0812c0c2b1323ce6b2fd9
BuyPvaAccount <info.buypva@gmail.com>
2025-12-15 03:44:57 -0800

    Harden PM2 restart policy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,10 @@ module.exports = {
     exec_mode: 'fork',
     instances: 1,
     autorestart: true,
+    min_uptime: '10s',
+    max_restarts: 20,
+    restart_delay: 2000,
+    exp_backoff_restart_delay: 1000,
     watch: false,
     max_memory_restart: '1G',
     env: {
